e3cf66432553f96e34a8692e70a9b982d3165902
seuntjens <jonas.Seuntjens@gmail.com>
2013-11-26 14:44:15 +0100

    Naam wordt opgezocht in adresboek bij het aankomen van een bericht wordt er ipv een dialog een layout geopend die dat we verder kunnen customizen cancel button werkt ook nu, reply button werkt maar functie stuurt gewoon hetzelfde bericht nog is terug.

--- Popup/src/sms/example/popup/PopSMSActivity.java
+++ Popup/src/sms/example/popup/PopSMSActivity.java
@@ -2,9 +2,20 @@ package sms.example.popup;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract.PhoneLookup;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 public class PopSMSActivity extends Activity {
 	
@@ -12,9 +23,7 @@ public class PopSMSActivity extends Activity {
 	   @Override
 	   public void onCreate(Bundle savedInstanceState) {
 	       super.onCreate(savedInstanceState);
-	       // no need for XML layouts right now
-	       // we will use a dialog instead
-	       //setContentView(R.layout.main); 
+	       setContentView(R.layout.activity_smsreceiver);
 
 	       // retrieve Serializable sms message object
 	       // by the key "msg" used to pass it
@@ -29,10 +38,58 @@ public class PopSMSActivity extends Activity {
 			 msg.setTimestamp( System.currentTimeMillis() );
 			 msg.setBody(" this is a test SMS message!");
 	       }
-	       showDialog(msg);
+	       init(msg);
+	       //showDialog(msg);
 	   }
 	   
-	   private void smsReply(String sender, String body){
+	   
+	   private void init(final PopMessage msg) {
+		   final String sender = quickCallerId(msg.getSender());
+		   final String body = msg.getBody();
+		   final String date = msg.getShortDate( msg.getTimestamp() );
+		   
+		   TextView message = (TextView) findViewById(R.id.messageText);
+		   TextView dateText = (TextView) findViewById(R.id.dateText);
+		   TextView phone = (TextView) findViewById(R.id.phoneText);
+		   
+		   message.setText(body);	
+		   phone.setText(sender);
+		   dateText.setText(date);
+		   
+		   Button cancel = (Button) findViewById(R.id.cancelButton);
+		   cancel.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				goHome();				
+			}
+		   });
+		   
+		   Button reply = (Button) findViewById(R.id.replyButton);
+		   reply.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				smsReply(msg.getSender(),body);				
+			}
+		   });
+			
+	}
+	   //telefoonnummer opzoeken in contacten
+	   private String quickCallerId(String phoneNumber){
+		    Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+		    ContentResolver resolver=getContentResolver();
+		    Cursor cur = resolver.query(uri, new String[]{PhoneLookup.DISPLAY_NAME}, null, null, null);
+		    if(cur!=null&&cur.moveToFirst()){
+		            String value=cur.getString(cur.getColumnIndex(PhoneLookup.DISPLAY_NAME));
+		            if(value!=null){ 
+		                cur.close();
+		                return value;
+		            }
+		    }
+		    cur.close();
+		    return "";
+		}
+
+	private void smsReply(String sender, String body){
 			Intent sendIntent = new Intent(Intent.ACTION_VIEW);
 			sendIntent.putExtra("address", sender);
 			sendIntent.putExtra("sms_body", body);
@@ -47,6 +104,7 @@ public class PopSMSActivity extends Activity {
 		    startActivity(intent);
 		    this.finish();
 		   }
+
 	   private void showDialog(PopMessage msg){
 
 			final String sender = msg.getSender();
@@ -55,9 +113,28 @@ public class PopSMSActivity extends Activity {
 			final String display = sender + "\n"
 		            + msg.getShortDate( msg.getTimestamp() )+ "\n"
 		            + body + "\n";
+			 /*AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			    // Get the layout inflater
+			    LayoutInflater inflater = this.getLayoutInflater();
+
+			    // Inflate and set the layout for the dialog
+			    // Pass null as the parent view because its going in the dialog layout
+			    builder.setView(inflater.inflate(R.layout.activity_smsreceiver, null))
+			    // Add action buttons
+			           .setPositiveButton(R.string.Reply, new DialogInterface.OnClickListener() {
+			               @Override
+			               public void onClick(DialogInterface dialog, int id) {
+			                   smsReply(sender,body);
+			               }
+			           })
+			           .setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
+			               public void onClick(DialogInterface dialog, int id) {
+			                   goHome();
+			               }
+			           });   */   
 
 		    // Display in Alert Dialog
-			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			/*AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage(display)
 			.setCancelable(false)
 			.setPositiveButton("Reply", new DialogInterface.OnClickListener() {
@@ -73,6 +150,6 @@ public class PopSMSActivity extends Activity {
 				}			  
 			});
 			AlertDialog alert = builder.create();
-			alert.show();
+			alert.show();*/
 	}
 }
